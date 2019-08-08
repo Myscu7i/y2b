09cb8fd75e2e58a9f407b3fee00590df64865d5b
Myscu7i <53902769+Myscu7i@users.noreply.github.com>
2019-08-08 19:46:32 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyCVkRcSiF0KmLCwmoo4edtw0vnSykCvFcc');
+define('APIKEY', 'AIzaSyBtidzCPFXqlgCrJH5VzAYpW41GQOZwGDs');
 define('GJ_CODE', 'US');
 define('SITE_NAME', 'y2b');
 define('TITLENAME', 'y2b');
 define('EN2DEKEY', '32123wwerwer323tgfdgds');
-define('EMAIL', '1334347212@qq.com');
+define('EMAIL', '3315147049@qq.com');
 define('NAME', 'admin');
-define('PASSWORD', 'admin');
+define('PASSWORD', '06155');
 define('LOGINHTML', '<!DOCTYPE html>
 <html lang="en">
 <head>
